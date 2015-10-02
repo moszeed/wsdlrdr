c91da6cea3a522c49a8df21ee430171d8d52d097
Moszeed <moszeed@googlemail.com>
2015-10-02 15:08:31 +0200

    tests extended

--- tests/tests.js
+++ tests/tests.js
@@ -5,16 +5,12 @@
     var Wsdlrdr = require('../src/index.js');
 
     var wsdlUrls = [
-        [{ host: 'www.predic8.com:8080', wsdl: '/base/IDService?wsdl' }, { secure: false }],
-        [{ host: 'www.webservicex.net', wsdl: '/globalweather.asmx?WSDL' }, { secure: false }],
-        [{ host: 'soaptest.parasoft.com', wsdl: '/calculator.wsdl' }, { secure: false }],
-        [{ host: 'webservices.oorsprong.org', wsdl: '/websamples.countryinfo/CountryInfoService.wso?WSDL'},
-         {secure: false}],
-        [{ host: 'www.dat.de:80', wsdl: '/VehicleRepairOnline/services/VehicleIdentificationService?wsdl' },
-         { secure: false }],
-        [{ host: 'www.car-copy.com', wsdl: '/ws/caratlas/ca_v2_soap.php?class=CAWS_CA&wsdl' }, {}],
-        [{ host: 'webservices.daehosting.com', wsdl: '/services/isbnservice.wso?WSDL'}, { secure: false }],
-        [{ host: 'www.dataaccess.com', wsdl: '/webservicesserver/numberconversion.wso?WSDL'}, { secure: false}]
+        [{ host: 'www.predic8.com:8080', wsdl: '/base/IDService?wsdl' }],
+        [{ host: 'www.webservicex.net', wsdl: '/globalweather.asmx?WSDL' }],
+        [{ host: 'soaptest.parasoft.com', wsdl: '/calculator.wsdl' }],
+        [{ host: 'webservices.oorsprong.org', wsdl: '/websamples.countryinfo/CountryInfoService.wso?WSDL'}],
+        [{ host: 'webservices.daehosting.com', wsdl: '/services/isbnservice.wso?WSDL'}],
+        [{ host: 'www.dataaccess.com', wsdl: '/webservicesserver/numberconversion.wso?WSDL'}]
     ];
 
     function executeSequentially(promiseFactories) {
@@ -28,6 +24,26 @@
 
 
     var test = require('tape');
+
+        test('getNamespaces', function(t) {
+
+            var promiseFactory = [];
+
+            // build promiseFactory
+            wsdlUrls.forEach((wsdlParams) => {
+
+                promiseFactory.push(() => {
+                    return Wsdlrdr.getNamespaces(wsdlParams[0], wsdlParams[1])
+                        .then((data) => {
+                            t.ok(data.length !== 0, wsdlParams[0].host + ' has ' + data.length + ' namespaces');
+                        });
+                });
+            });
+
+            executeSequentially(promiseFactory)
+                .then(() => { t.end(); })
+                .catch((err) => { console.log(err); })
+        });
 
         test('getAllFunctions', function(t) {
 
@@ -46,7 +62,7 @@
             });
 
             executeSequentially(promiseFactory)
-                .then(() => { t.end() })
+                .then(() => { t.end(); })
                 .catch((err) => { console.log(err); })
         });
 
@@ -57,7 +73,7 @@
             // build promiseFactory
             wsdlUrls.forEach((wsdlParams) => {
 
-                var methodNames = wsdlParams[2];
+                var methodNames = wsdlParams[2] || wsdlParams[1];
                 if (!methodNames) {
                     t.end('no method Names');
                 }
@@ -75,9 +91,6 @@
                         });
                     }
                 );
-
-
-
             });
 
             executeSequentially(promiseFactory)
@@ -85,6 +98,30 @@
                 .catch((err) => { console.log(err); })
         });
 
+        test('getXmlDataAsJson', function(t) {
+
+            t.plan(2);
+
+            var responseXml = '<?xml version="1.0" encoding="UTF-8"?>' +
+                                '<SOAP-ENV:Envelope ' +
+                                    'xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/" ' +
+                                    'xmlns:wsdl="http://schemas.xmlsoap.org/" ' +
+                                    'xmlns:xsd="http://www.w3.org/2001/" ' +
+                                    'xmlns:tns="http://predic8.com/wsdl/" ' +
+                                    'xmlns:soap="http://schemas.xmlsoap.org/wsdl/" ' +
+                                '>' +
+                                '<SOAP-ENV:Body>' +
+                                    '<tns:testResponseItem1>123</tns:testResponseItem1>' +
+                                    '<tns:testResponseItem2>123</tns:testResponseItem2>' +
+                                '</SOAP-ENV:Body>' +
+                            '</SOAP-ENV:Envelope>';
+
+            var dataAsJson = Wsdlrdr.getXmlDataAsJson(responseXml);
+                dataAsJson.forEach((dataItem) => {
+                    if (dataItem.testResponseItem1) t.pass('testResponseItem1 is available');
+                    if (dataItem.testResponseItem2) t.pass('testResponseItem2 is available');
+                });
+        });
 
 
 })();
